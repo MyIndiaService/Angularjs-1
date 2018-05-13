982ebac247233490e300c202f7c998db803edce1
MyIndiaService <36450797+MyIndiaService@users.noreply.github.com>
2018-05-13 15:43:38 +0530

    Angular-ng-Click-Accordian
    Angular-ng-Click-Accordian 
    display the content according left side bar list filter and count

---
+++ DemoNg-click-Directive-4/js/example.js
@@ -0,0 +1,44 @@
+var app=angular.module("mainApp",[]);
+
+app.controller("userController",function($scope){
+    $scope.users=[
+        {id:1,name:"Amit",Gender:"Male",City:"Delhi"},
+        {id:2,name:"smit",Gender:"Male",City:"Delhi"},
+        {id:3,name:"Ramesh",Gender:"Male",City:"Delhi"},
+        {id:4,name:"Annu",Gender:"Female",City:"Noida"},
+        {id:5,name:"Priyanka",Gender:"Female",City:"Lakshmi Nager"}
+    ];
+    
+    $scope.DisplayCity="";
+    
+    $scope.delhiCount=0;
+    $scope.noidaCount=0;
+    $scope.laximiCount=0;
+    
+    $scope.getCityCount=function(cityName)
+    {
+        var count=0;
+        console.log(cityName);
+        $scope.users.filter(function(user){
+            
+            if(user.City==cityName)
+            {
+               count++;     
+            }
+            
+        });
+        return count;
+    }
+    
+    $scope.ChangeCity=function(cityName)
+    {
+        $scope.DisplayCity=cityName;
+    }
+    
+     $scope.citys=[
+         {id:1,cityName:"Delhi"},
+         {id:2,cityName:"Noida"},
+         {id:3,cityName:"Lakshmi Nager"}
+     ];
+    $scope.count=0;
+});
